66cec8205f18ce5189178a8dd4eff6a51cb86272
Javasiete <165825352+javasiete@users.noreply.github.com>
2024-06-23 14:43:08 -0300

    v 1.0

--- JS/contadorCatan.js
+++ JS/contadorCatan.js
@@ -1,15 +1,97 @@
-// Funciones para incrementar y decrementar los contadores
+// Funciones para INCREMENTAR y DECREMENTAR los CONTADORES:
 function incrementar(nombreContador) {
-    let contador = document.getElementById(nombreContador);
-    let valor = parseInt(contador.textContent);
-    contador.textContent = valor + 1;
-  }
+  let contador = document.getElementById(nombreContador);
+  let valor = parseInt(contador.textContent);
+  contador.textContent = valor + 1;
+  actualizarEstilo();
+}
 
-  function decrementar(nombreContador) {
-    let contador = document.getElementById(nombreContador);
-    let valor = parseInt(contador.textContent);
-    if (valor > 0) {
-      contador.textContent = valor - 1;
-    }
+function decrementar(nombreContador) {
+  let contador = document.getElementById(nombreContador);
+  let valor = parseInt(contador.textContent);
+  if (valor > 0) {
+    contador.textContent = valor - 1;
+    actualizarEstilo();
   }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//  Funcion del BOTON REINICIAR CABALLEROS
+document.addEventListener('DOMContentLoaded', function() {
+
+var botonReiniciar_fCaballeros = document.getElementById('reiniciar_F.Caballeros');
+var divCaballeros = document.getElementById('f.caballeros');
+
+botonReiniciar_fCaballeros.addEventListener('click', function() {
+  divCaballeros.textContent = "0"; //El contador de F.Caballeros vuelve a 0.
+  divCaballeros.classList.remove('green-text');
+  divCaballeros.classList.add('red-text');
+  div_resumen.textContent = 'Están Perdiendo';
+});
+});
+//  FIN DE > Funcion del BOTON REINICIAR CABALLEROS
+
+//----------------------------------------------------------------------------------------------------------------------
+// FUNCION PARA COLORES SEGUN LA FUERZA BARBARA vs FUERZA CABALLEROS ACTIVADOS:
+
+var div_fuerza_caballeros = document.getElementById('div_fuerza_caballeros');
+var div_resumen = document.getElementById('resumen');
+
+// > Función para actualizar el estilo de los divs según la comparación de valores
+function actualizarEstilo() {
+  var valorBarbara = parseInt(document.getElementById('f.barbara').textContent);
+  var valorCaballeros = parseInt(document.getElementById('f.caballeros').textContent);
+
+  var divBarbara = document.getElementById('f.barbara');
+  var divCaballeros = document.getElementById('f.caballeros');
+
+  if (valorCaballeros >= valorBarbara) {
+      divCaballeros.classList.add('green-text');
+      divCaballeros.classList.remove('red-text');
+      div_resumen.textContent = 'Están Ganando';
+  } else {
+      divCaballeros.classList.remove('green-text');
+      divCaballeros.classList.add('red-text');
+      div_resumen.textContent = 'Están Perdiendo';
+      
+  }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// BOTON REINICIAR EL JUEGO:
+
+// > Asignamiento de los Contadores:
+var divNaranja = document.getElementById('j.naranja');
+var divAzul = document.getElementById('j.azul');
+var divRojo = document.getElementById('j.rojo');
+var divBlanco = document.getElementById('j.blanco');
+var divFuerzaBarbara = document.getElementById('f.barbara');
+var divFuerzaCaballeros = document.getElementById('f.caballeros');
+
+var botonBorrar = document.getElementById('reiniciar'); //Asignamiento al Btn Resetear
+
+// > La Función que sucede al apretar el Btn:
+
+botonBorrar.addEventListener('click', function() {
+
+  var div_fuerza_caballeros = document.getElementById('div_fuerza_caballeros');
+  var div_resumen = document.getElementById('resumen');
+  var divBarbara = document.getElementById('f.barbara');
+  var divCaballeros = document.getElementById('f.caballeros');
+
+  var respuesta = window.confirm('¿Deseas reiniciar el juego"?');
+  if (respuesta) {
+    divNaranja.textContent = "3";
+    divAzul.textContent = "3";
+    divRojo.textContent = "3";
+    divBlanco.textContent = "3";
+    divFuerzaBarbara.textContent = "4";
+    divFuerzaCaballeros.textContent = "0";
+    
+    divCaballeros.classList.remove('green-text');
+    divCaballeros.classList.add('red-text');
+    div_resumen.textContent = 'Están Perdiendo';
+  }else {}  
+});
+
 
